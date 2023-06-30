e51070edb3028027d011441921b6d6e2d83c4a43
Ivan-Shalnov <98489312+Ivan-Shalnov@users.noreply.github.com>
2023-07-01 01:31:17 +0200

    svgr config

--- next.config.js
+++ next.config.js
@@ -1,29 +1,66 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
-    // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find(rule =>
-      rule.test?.test?.('.svg'),
-    );
+    // SVGR
+    {
+      // Grab the existing rule that handles SVG imports
+      const fileLoaderRule = config.module.rules.find(rule =>
+        rule.test?.test?.('.svg'),
+      );
 
-    config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      // Convert all other *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: /url/ }, // exclude if *.svg?url
-        use: ['@svgr/webpack'],
-      },
-    );
+      config.module.rules.push(
+        // Reapply the existing rule, but only for svg imports ending in ?url
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/, // *.svg?url
+        },
+        // Convert all other *.svg imports to React components
+        {
+          test: /\.svg$/i,
+          issuer: /\.[jt]sx?$/,
+          resourceQuery: { not: /url/ }, // exclude if *.svg?url
+          use: [
+            {
+              loader: '@svgr/webpack',
+              options: {
+                svgoConfig: {
+                  plugins: [
+                    {
+                      name: 'preset-default',
+                      params: {
+                        overrides: {
+                          removeViewBox: false,
+                        },
+                      },
+                    },
+                    {
+                      name: 'prefixIds',
+                      active: true,
+                    },
+                    {
+                      name: 'removeDimensions',
+                      active: true,
+                    },
+                    {
+                      name: 'removeViewBox',
+                      active: false,
+                    },
+                  ],
+                  removeViewBox: false,
+                  removeDimensions: true,
+                  typescript: true,
+                  ref: true,
+                },
+              },
+            },
+          ],
+        },
+      );
 
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i;
+      // Modify the file loader rule to ignore *.svg, since we have it handled now.
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
 
     return config;
   },
