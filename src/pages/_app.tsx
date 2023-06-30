d0779be9dc6511633b03c87e01fe051d928d6951
Ivan-Shalnov <98489312+Ivan-Shalnov@users.noreply.github.com>
2023-07-01 01:40:04 +0200

    add hook useSetVPVars

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import 'reset-css-complete/reset.css';
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
+import useSetVPVars from '@/hooks/use-set-vp-vars';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useSetVPVars();
   return <Component {...pageProps} />;
 }
